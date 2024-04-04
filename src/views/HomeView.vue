0f8eb35c16047133d8b2be7ab8a3aac2701d316b
Ratnesh Patil <ratneshdesigns@gmail.com>
2024-04-05 00:33:03 +0530

    import primvue ui library

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import Button from 'primevue/button';
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <Button>Hi I am button</Button>
   </main>
 </template>
